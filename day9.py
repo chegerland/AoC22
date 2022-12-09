2557e7168d7386c05baae42d22b58da398941cf5
Christoph Egerland <christoph.egerland@pwc.com>
2022-12-09 14:51:42 +0100

    complete day 9

--- day9.py
+++ day9.py
@@ -13,31 +13,29 @@ def main():
     f = open("inputs/day9.txt", "r")
     instructions = [(instruction[0], int(instruction[2:])) for instruction in f.read().split("\n")]
 
-    H = np.array([0, 0])
-    T = np.array([0, 0])
+    rope = [np.array([0, 0]) for _ in range(10)]
 
-    visited = set()
+    visited_1 = set()
+    visited_2 = set()
 
     for (direction, steps) in instructions:
 
-
         for _ in range(steps):
-            H += encoding[direction]
+            rope[0] += encoding[direction]
+            
+            for id in range(1, len(rope)):
+                if rope[id - 1][0] - 1 <= rope[id][0] <= rope[id - 1][0] + 1 and rope[id - 1][1] - 1 <= rope[id][1] <= rope[id - 1][1] + 1:
+                    pass
+                else: 
+                    difference = rope[id - 1] - rope[id]
+                    step_direction = np.array([x if abs(x) < 2 else np.sign(x) for x in difference])
+                    rope[id] += step_direction
 
-            if H[0] - 1 <= T[0] <= H[0] + 1 and H[1] - 1 <= T[1] <= H[1] + 1:
-                pass
-            else: 
-                difference = H - T
-                step_direction = np.array([x if abs(x) < 2 else np.sign(x) for x in difference])
-                print(difference, step_direction)
-                T += step_direction
+            visited_1.add((rope[1][0], rope[1][1]))
+            visited_2.add((rope[9][0], rope[9][1]))
 
-            #print(H, T)
-            visited.add((T[0], T[1]))
-
-    print(visited) 
-    print(len(visited)) 
-
+    print(len(visited_1)) 
+    print(len(visited_2)) 
 
 
 if __name__ == "__main__":
